06f56375d22b698e4d09a97f2442af9edce568aa
Mike Zimmerman <mike.zimmerman@vivint.com>
2024-01-25 15:02:11 -0700

    Another attempt.

--- apod_service.py
+++ apod_service.py
@@ -124,7 +124,7 @@ def callback():
     
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
-        authorization_response=request.url,
+        authorization_response='https://apod.mikezim.org/login',
         redirect_url='https://apod.mikezim.org/login/callback',
         code=code
     )

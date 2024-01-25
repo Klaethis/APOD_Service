f1f7e9dc2374ac266b6b4a23216c4a2ab64145e0
Mike Zimmerman <mike.zimmerman@vivint.com>
2024-01-25 15:00:07 -0700

    Trying to fix redirect_url

--- apod_service.py
+++ apod_service.py
@@ -125,7 +125,7 @@ def callback():
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
-        redirect_url=request.base_url,
+        redirect_url='https://apod.mikezim.org/login/callback',
         code=code
     )
     token_response = requests.post(
